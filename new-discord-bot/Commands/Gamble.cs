be731e44188a467769562d021a8c8f53c88c4f4e
Nicolai <pjevert03@gmail.com>
2024-12-06 23:37:36 +0100

    Refactor Gamble command and add new methods
    Refactored the `Execute` method in `Gamble.cs` to streamline the command execution flow by removing slot name validation and focusing on creating a gambling channel. Added `CreateGambleChannel`, `GetComponent`, and `GetEmbed` methods to handle channel creation, component building, and embed message construction, respectively. Removed commented-out code related to slot name validation.

--- new-discord-bot/Commands/Gamble.cs
+++ new-discord-bot/Commands/Gamble.cs
@@ -18,29 +18,91 @@ namespace new_discord_bot.Commands
 
 		public async Task Execute(SocketSlashCommand command)
 		{
-			object data = command.Data.Options.First().Value;
-			string? slotName = data.ToString();
+			SocketTextChannel channel;
 
-			if(slotName == null)
+			if (command.Channel is SocketTextChannel textChannel)
 			{
-				await command.RespondAsync("Invalid slot name");
-				throw new Exception("Invalid slot name?");
+				channel = await this.CreateGambleChannel(command);
 			}
+			else
+			{
+				await command.RespondAsync("This command can only be used in a text channel");
+				return;
+			}
+
+			await channel.SendMessageAsync(embed: this.GetEmbed(command).Build(), components: this.GetComponent(command).Build());
+			await command.RespondAsync("Created thread!");
+			//object data = command.Data.Options.First().Value;
+			//string? slotName = data.ToString();
+
+			//if(slotName == null)
+			//{
+			//	await command.RespondAsync("Invalid slot name");
+			//	throw new Exception("Invalid slot name?");
+			//}
+
+			//try
+			//{
+			//	_slots.TryGetValue(slotName, out ISlot? slot);
+			//	if (slot != null)
+			//	{
+			//		await slot.Execute(command);
+			//		return;
+			//	}
+			//}
+			//catch (Exception ex)
+			//{
+			//	Console.WriteLine(ex.Message);
+			//	throw new Exception("An error occurred while executing the command");
+			//}
+		}
+
+
+		private ComponentBuilder GetComponent(SocketSlashCommand command)
+		{
+			SelectMenuBuilder menuBuilder = new SelectMenuBuilder()
+				.WithPlaceholder("Select a slot machine")
+				.WithCustomId("slot-selector")
+				.WithMinValues(1)
+				.WithMaxValues(1)
+				.AddOption("🍭 Sweet Bonanza", "bonanza", "Load up on sugar in Sweet Bonanza™")
+				.AddOption("Option B", "opt-b", "Option B bla bla");
+
+			ComponentBuilder builder = new ComponentBuilder()
+				.WithSelectMenu(menuBuilder);
+
+			return builder;
+		}
+
+
+		private EmbedBuilder GetEmbed(SocketSlashCommand command)
+		{
+			EmbedBuilder embedBuilder = new EmbedBuilder()
+				.WithTitle("Slot Selector")
+				.WithDescription("Select the slot you wish to gamble on 😈")
+				.WithFooter(command.User.Username, command.User.GetAvatarUrl())
+				.WithColor(Color.Blue);
+
+			return embedBuilder;
+		}
+
+		private async Task<SocketTextChannel> CreateGambleChannel(SocketSlashCommand command)
+		{
+			SocketTextChannel thread;
 
 			try
 			{
-				_slots.TryGetValue(slotName, out ISlot? slot);
-				if (slot != null)
-				{
-					await slot.Execute(command);
-					return;
-				}
+				SocketTextChannel channel = (SocketTextChannel)command.InteractionChannel;
+				thread = await channel.CreateThreadAsync(command.User.Username + "'s gamble thread", autoArchiveDuration: ThreadArchiveDuration.ThreeDays);
 			}
 			catch (Exception ex)
 			{
 				Console.WriteLine(ex.Message);
-				throw new Exception("An error occurred while executing the command");
+				await command.RespondAsync("An error occurred while creating the gamble channel");
+				throw new Exception("An error occurred while creating the gamble channel");
 			}
+
+			return thread;
 		}
 
 		public SlashCommandProperties Create()

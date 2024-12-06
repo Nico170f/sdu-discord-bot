de7d99537168d3f86b05c05ddd706ad4dda942ed
Nicolai <pjevert03@gmail.com>
2024-12-06 21:30:21 +0100

    Add missing file

---
+++ new-discord-bot/Config/Config.cs
@@ -0,0 +1,11 @@
+﻿using Discord;
+using Newtonsoft.Json;
+
+namespace new_discord_bot.Configuration
+{
+	public class Colors
+	{
+		public static Color Green = Color.Green;
+		public static Color Red = Color.Red;
+	}
+}

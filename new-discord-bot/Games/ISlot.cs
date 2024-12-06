5a9fe34cbb5f65836e3369e25227976b9e023ef8
Nicolai <pjevert03@gmail.com>
2024-12-06 21:27:34 +0100

    Changes

---
+++ new-discord-bot/Games/ISlot.cs
@@ -0,0 +1,18 @@
+﻿using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace new_discord_bot.Games
+{
+	public interface ISlot
+	{
+		public string Name { get; }
+		public string Description { get; }
+		public int Width { get; }
+		public int Height { get; }
+		Task Execute(SocketSlashCommand command);
+	}
+}

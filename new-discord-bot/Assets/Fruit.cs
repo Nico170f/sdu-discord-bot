8305df539e2eb3ce5074021fc4340eac2d686338
Rasmus <liltorp03@gmail.com>
2024-12-16 22:49:13 +0100

    created Item.cs abstract class. Made Fruit.cs inherit from Item.cs

--- new-discord-bot/Assets/Fruit.cs
+++ new-discord-bot/Assets/Fruit.cs
@@ -1,38 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace new_discord_bot.Assets
+﻿namespace new_discord_bot.Assets
 {
-	public class Fruit
+	public class Fruit : Item
 	{
-		public string Name { get; }
-		public int Value { get; }
-		public int Rarity { get; }
-		public string Emoji { get; set; } //Remove set
 		public string Letter { get; }
-		public Dictionary<int, double> Payouts { get; } // Count thresholds -> payout amounts
-
-		public Fruit(string name, int value, int rarity, string emoji, string letter, Dictionary<int, double> payouts )
+		public Fruit(string name, int value, int rarity,string letter, string emoji, Dictionary<int, double> payouts) : base(name, value, rarity, emoji, payouts)
 		{
-			Name = name;
-			Value = value;
-			Rarity = rarity;
-			Emoji = emoji;
 			Letter = letter;
-			Payouts = payouts;
-		}
-
-		public double GetPayout(int count, double spinPrice)
-		{
-			double basePayout = 0;
-			if (count >= 12) basePayout = Payouts[12];
-			else if (count >= 10) basePayout = Payouts[10];
-			else if (count >= 8) basePayout = Payouts[8];
-
-			return basePayout * spinPrice; // Scale payout based on spin price
 		}
 	}
 }
---
+++ new-discord-bot/Assets/Item.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace new_discord_bot.Assets
+{
+    public abstract class Item
+    {
+        public string Name { get; }
+        public int Value { get; }
+        public int Rarity { get; }
+        public string Emoji { get; }
+        
+        public Dictionary<int, double> Payouts { get; }
+        public Item(string name, int value, int rarity, string emoji, Dictionary<int, double> payouts)
+        {
+            Name = name;
+            Value = value;
+            Rarity = rarity;
+            Emoji = emoji;
+            Payouts = payouts;
+        }
+        public double GetPayout(int count, double spinPrice)
+        {
+            double basePayout = 0;
+            if (count >= 12) basePayout = Payouts[12];
+            else if (count >= 10) basePayout = Payouts[10];
+            else if (count >= 8) basePayout = Payouts[8];
+
+            return basePayout * spinPrice; // Scale payout based on spin price
+        }
+    }
+}
